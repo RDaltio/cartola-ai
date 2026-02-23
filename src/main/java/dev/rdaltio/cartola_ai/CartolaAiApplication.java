196f7352f79627bdafd71addc1a6199fdebf4ac8
Rafael Daltio <rdaltio.dev@gmail.com>
2026-02-23 00:21:34 -0300

    feat: setup initial infra with spring boot, docker and postgres

---
+++ src/main/java/dev/rdaltio/cartola_ai/CartolaAiApplication.java
@@ -0,0 +1,13 @@
+package dev.rdaltio.cartola_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartolaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartolaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rdaltio/cartola_ai/CartolaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rdaltio.cartola_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartolaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
